8894f9701d848ecfaef549cfb7017d14c95d232d
juliocervantes18 <julio18.cervantes@gmail.com>
2025-02-03 13:58:26 -0700

    cartas poker

--- src/Poker.java
+++ src/Poker.java
@@ -1,2 +1,10 @@
 public class Poker {
+
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+        deck.shuffle();
+        deck.head();
+        deck.pick();
+        deck.hand();
+    }
 }
