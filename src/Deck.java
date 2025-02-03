6e7946d4c42f100eff43ff73bd6c08190ee35a9b
juliocervantes18 <julio18.cervantes@gmail.com>
2025-02-03 15:11:16 -0700

    cartas poker

--- src/Deck.java
+++ src/Deck.java
@@ -22,8 +22,8 @@ class Deck {
 
     public void head() {
         if (!cartas.isEmpty()) {
-            System.out.println(cartas.remove(0).toString());
-            System.out.println("Quedan " + cartas.size() + " cartas en el deck.");
+            System.out.println("Se retiro esta carta" + " " + cartas.remove(0).toString());
+            System.out.println("Y quedan " + cartas.size() + " cartas en el deck.");
         } else {
             System.out.println("No quedan cartas en el deck.");
         }
